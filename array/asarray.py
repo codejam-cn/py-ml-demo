dbcd286290b25b52d3a00b599e8df8210c7fe3f4
赵森森 <451682479@qq.com>
2022-09-07 20:20:04 +0800

    .

---
+++ array/asarray.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# numpy.asarray(a,dtype=None,order=None)
+# a 任意形式输入参数
+# dtype
+# order
+
+arr = np.array([1, 2, 3, 4, 56, 7])
+arr2 = np.asarray(arr)
+print(arr2)
+
+
+arr3 = np.asarray([(1, 2.3,0), (4, 5)])
+print(arr3)
