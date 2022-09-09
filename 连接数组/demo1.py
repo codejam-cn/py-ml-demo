68902830707ce937b94fc817ac4f41078cbbda45
赵森森 <451682479@qq.com>
2022-09-09 14:16:27 +0800

    连接数组 stack

--- 连接数组/demo1.py
+++ 连接数组/demo1.py
@@ -8,3 +8,20 @@ print(y)
 
 z = np.concatenate((x, y))
 print(z)
+
+# stack
+
+a = np.arange(1, 5).reshape(2, 2)
+print(a)
+print(a.shape)
+print(a.ndim)
+
+b = np.arange(5, 9).reshape(2, 2)
+print(b)
+print(b.shape)
+print(b.ndim)
+
+c = np.stack((a, b))
+print(c)
+print(c.shape)
+print(c.ndim)
