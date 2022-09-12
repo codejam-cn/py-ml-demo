35578cc1ee3f3ffdf1cf1e3524afb6d4d886afe1
赵森森 <451682479@qq.com>
2022-09-12 22:14:24 +0800

    实战

--- 项目/kobe.py
+++ 项目/kobe.py
@@ -11,6 +11,7 @@ ncols = sheet0.ncols
 
 print(nrows)
 
+# text:'赛季'text:'球队'text:'出场'text:'首发'text:'时间'text:'投篮'text:'命中'text:'出手'text:'三分'text:'命中'text:'出手'text:'罚球'text:'命中'text:'出手'text:'篮板'text:'前场'text:'后场'text:'助攻'text:'抢断'text:'盖帽'text:'失误'text:'犯规'text:'得分'text:'胜'text:'负'
 for row in range(nrows):
     for col in range(ncols):
         print(sheet0.cell(row, col), end='')
